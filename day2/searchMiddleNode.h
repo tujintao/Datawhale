799f00741c15dec97827f31992a5fd83464eaf95
voidspiral <cs-shop@163.com>
2019-02-28 21:26:20 +0800

    MiddleNode

---
+++ day2/searchMiddleNode.h
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        //利用双指针让快指针的速度是慢指针的两倍
+        if (!head || !head->next) return head;
+        ListNode* slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
