6a295e7497183d408842d7081c2c7e370ddd156e
voidspiral <cs-shop@163.com>
2019-03-12 10:00:17 +0800

    update delete

--- task5/BTree.cc
+++ task5/BTree.cc
@@ -54,34 +54,20 @@ public:
     } else if (key < root->val) {
       root->left = deleteNode(root->left, key);
     } else {
-      TreeNode* new_root = nullptr;
-      if (root->left == nullptr) {
-        new_root = root->right;
-      } else if (root->right == nullptr) {
-        new_root = root->left;
+      if (root->left != nullptr && root->right != nullptr) {
+        TreeNode* min = root->right;
+        while (min->left != nullptr) min = min->left;
+        root->val = min->val;
+        root->right = deleteNode(root->right, min->val);
       } else {
-        //找到右子树中的最小节点
-        TreeNode* parent = root;
-        new_root = root->right;
-        while (new_root->left != nullptr) {
-          parent = new_root;
-          new_root = new_root->left;
-        }
-        
-        if (parent != root) {
-          parent->left = new_root->right;
-          new_root->right = root->right;
-        }
-        
-        new_root->left = root->left;      
+        TreeNode* new_root = root->left == nullptr ? root->right : root->left;
+        root->left = root->right = nullptr;
+        delete root;
+        return new_root;
       }
-      
-      delete root;
-      return new_root;
-    }
-    
+    }    
     return root;
-  }
+    }
     // void dfs(TreeNode* root){
     //     if (!root) return;
     //     dfs(root->left);
