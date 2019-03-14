6e3ac77dd86670b692bab0b9cc798329810bad75
voidspiral <cs-shop@163.com>
2019-03-14 09:35:39 +0800

    heap + dij

--- task6/dijkstra.cc
+++ task6/dijkstra.cc
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
 using namespace std;
 //参考挑战程序设计竞赛
 const int maxn = 1e5;
@@ -27,7 +30,33 @@ void dijkstra(int s){
     }
 } 
 //用堆优化查找下一个使用的顶点的最短距离
-
+struct edge{
+    int to, cost;
+};
+typedef pair<int, int> P;//first 最短距离 second是
+vector<edge> G[maxn];
+int d2[maxn];
+void dijkstra2(int s) {
+    priority_queue<P, vector<P>, greater<P> > que;
+    fill(d2, d2 + V, INF);
+    d2[s] = 0;
+    que.push(P(0, s));
+    while (que.size()) {
+        P p = que.top();
+        que.pop();
+        int u = p.second;
+        //当前不是最短距离的话
+        //也就是说u没有被访问过
+        if (d2[u] < p.first) continue;
+        for (int i = 0; i < G[u].size(); ++i) {
+            edge v = G[u][i];//前驱u 后继v
+            if (d2[v.to] > d2[u] + d2[v.cost]){
+                d2[v.to] = d2[u] + v.cost;
+                que.push(P(d[v.to], v.to));
+            }
+        }
+    }
+}
 int main()
 {
     return 0;
